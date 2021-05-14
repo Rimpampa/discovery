078bca2d7547b2c112df04956e837766866b9fd9
Riccardo Ripanti <riccardo.ripanti01@gmail.com>
2021-05-14 19:47:56 +0200

    My first embedded program

--- src/05-led-roulette/src/main.rs
+++ src/05-led-roulette/src/main.rs
@@ -2,14 +2,42 @@
 #![no_main]
 #![no_std]
 
-use aux5::entry;
+use aux5::{entry, Delay, DelayMs, LedArray, OutputSwitch};
+use volatile::Volatile;
 
 #[entry]
 fn main() -> ! {
-    let _y;
-    let x = 42;
-    _y = x;
+    let (mut delay, mut leds): (Delay, LedArray) = aux5::init();
 
-    // infinite loop; just so we don't leave this stack frame
-    loop {}
+    let mut step_time = 50_u8;
+    let v_step_time = Volatile::new(&mut step_time);
+
+    let mut distance = 1;
+    let mut v_distance = Volatile::new(&mut distance);
+
+    v_distance.write(v_distance.read().min(leds.len()));
+    for led in &mut leds[..v_distance.read()] {
+        led.on().ok();
+    }
+
+    let mut nxt_on = v_distance.read();
+    let mut nxt_off = 0;
+
+    loop {
+        delay.delay_ms(v_step_time.read());
+        leds[nxt_on].on().ok();
+
+        delay.delay_ms(v_step_time.read());
+        leds[nxt_off].off().ok();
+
+        nxt_on += 1;
+        if nxt_on == leds.len() {
+            nxt_on = 0;
+        }
+
+        nxt_off += 1;
+        if nxt_off == leds.len() {
+            nxt_off = 0;
+        }
+    }
 }
--- src/openocd.gdb
+++ src/openocd.gdb
@@ -1,5 +1,5 @@
 # Connect to gdb remote server
-target remote :3333
+target extended-remote :3333
 
 # Load will flash the code
 load
@@ -11,7 +11,7 @@ set print asm-demangle on
 set print pretty on
 
 # Disable style sources as the default colors can be hard to read
-set style sources off
+# set style sources off
 
 # Initialize monitoring so iprintln! macro output
 # is sent from the itm port to itm.txt
